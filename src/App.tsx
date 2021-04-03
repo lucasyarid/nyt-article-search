7f8c38447f2931a62b1d3259e91e6664197e65ed
Lucas Yarid <lucas.boccuzi@quintoandar.com.br>
2021-04-03 12:40:38 -0300

    add GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
-import React, { FC } from 'react'
+import React, { FC, Fragment } from 'react'
+import { GlobalStyle } from './GlobalStyle'
 import logo from './logo.svg'
-import './App.css'
 import { Title } from './styledComponentExample'
 
 const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Title>Test Styled Components</Title>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <GlobalStyle />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Title>Test Styled Components</Title>
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Fragment>
   )
 }
 
