52fb1665b07c0de9260eb5cbc4f0f3261a88fb47
Lucas Yarid <lucas.boccuzi@quintoandar.com.br>
2021-04-06 09:40:02 -0300

    add query to view

--- src/Article/views/Search/index.tsx
+++ src/Article/views/Search/index.tsx
@@ -1,87 +1,72 @@
-import React, { FC, useEffect, useState } from 'react'
+import React, { FC, useState } from 'react'
 import styled from 'styled-components'
-import { useHistory, useLocation } from 'react-router-dom'
 
 import { Dialog } from 'DesignSystem/atoms'
 import { InputSearch } from 'DesignSystem/organisms'
 import { CenteredTemplate } from 'DesignSystem/templates'
-import { fetchArticlesByQuery } from 'Article/api'
+import { useQueryArticles } from 'Article/hooks'
+import { useUrlQuery } from 'Base/router'
 
 export const Wrapper = styled.div`
   width: 750px;
 `
 
-const resultList = ['title 1', 'title 2', 'title 3', 'title 4']
-
 export const SearchView: FC = () => {
-  const history = useHistory()
-  const location = useLocation()
+  const { getQueryParamFromUrl, setUrlParam } = useUrlQuery()
+
+  const pageFromUrl = getQueryParamFromUrl('page')
+  const [page, setPage] = useState(Number(pageFromUrl))
+
+  const queryFromUrl = getQueryParamFromUrl('query') || ''
+  const [query, setQuery] = useState(queryFromUrl)
+
+  const { data, isLoading, error } = useQueryArticles(query, page.toString())
+  const [hasError, setHasError] = useState(false)
+  console.log(data)
+  if (error) setHasError(true)
 
   const [selected, setSelected] = useState(0)
-  const [hasError, setHasError] = useState(false)
-
-  const queryFromUrl = new URLSearchParams(location.search).get('q') as string
-  const pageFromUrl = new URLSearchParams(location.search).get('page') as string
-
-  const setUrlParam = (paramKey: string, paramValue: string) => {
-    const searchParams = new URLSearchParams(location.search)
-    searchParams.set(paramKey, paramValue)
-    history.push({
-      search: searchParams.toString(),
-    })
-  }
 
   const onDebounced = (query: string) => {
     setUrlParam('q', query)
-    fetchData(query)
+    setQuery(query)
   }
 
-  const fetchData = async (query?: string, page?: string) => {
-    const data = await fetchArticlesByQuery({
-      fq: 'document_type:("article")',
-      fl: ['_id', 'document_type', 'headline'],
-      q: query || queryFromUrl,
-      page: page || pageFromUrl,
+  const onClickNext = () =>
+    setPage((page) => {
+      const nextPage = page + 1
+      setUrlParam('page', nextPage.toString())
+      return nextPage
     })
-    console.log({ data })
-  }
 
-  useEffect(() => {
-    fetchData()
-  }, [])
+  const onClickPrevious = () =>
+    setPage((page) => {
+      if (!page) return page
+      const previousPage = page - 1
 
-  const onClickNext = () => {
-    const pageFromUrlNumber = Number(pageFromUrl) || 0
-    const nextPage = (pageFromUrlNumber + 1).toString()
-
-    setUrlParam('page', nextPage)
-    fetchData(undefined, nextPage)
-  }
-
-  const onClickPrevious = () => {
-    const pageFromUrlNumber = Number(pageFromUrl)
-    if (!pageFromUrlNumber) return
-    const nextPage = (pageFromUrlNumber - 1).toString()
-
-    setUrlParam('page', nextPage)
-    fetchData(undefined, nextPage)
-  }
+      setUrlParam('page', previousPage.toString())
+      return previousPage
+    })
 
   const onClose = () => setHasError(false)
 
   const headToArticle = () => console.log(selected)
 
+  const resultList = query
+    ? data?.response?.docs.map((doc) => doc.headline.main)
+    : []
+
   return (
     <CenteredTemplate backgroundSrc="https://www.nytimes.com/images/2021/02/19/books/review/Fajardo-Anstine2/Fajardo-Anstine2-videoSixteenByNine3000.jpg">
       <Wrapper>
         <InputSearch
-          isLoading={false}
+          isLoading={isLoading}
           name="search"
           placeholder="Search New York Times articles"
           onDebounced={onDebounced}
-          resultList={resultList}
-          value={queryFromUrl || ''}
-          delay={300}
+          resultList={resultList || []}
+          value={queryFromUrl}
+          delay={500}
           isFirstPage={!Number(pageFromUrl)}
           isLastPage={false}
           onClickNext={onClickNext}
@@ -94,8 +79,8 @@ export const SearchView: FC = () => {
       </Wrapper>
       <Dialog
         isVisible={hasError}
-        title="title"
-        content="content"
+        title="Sorry for that"
+        content="Seems like something went wrong on searching for articles"
         onClose={onClose}
       />
     </CenteredTemplate>
